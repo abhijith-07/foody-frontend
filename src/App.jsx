7433c063d5e56c9465f78d316726c0edde42a6b2
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-11 13:24:44 +0530

    fix: base url configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header"
 import FoodContainer from "./components/FoodContainer"
+import BASE_URL from "./config/url-fallback.json"
 
 function App() {
+  const {api} = BASE_URL 
+
   return (
     <>
       <Header />
-      <FoodContainer />
+      <FoodContainer api={api} />
     </>
   )
 }
