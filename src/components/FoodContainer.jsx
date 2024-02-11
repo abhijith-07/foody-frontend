888fdb5f0c3b7a85473c271b70eb4ed2db709586
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-11 23:39:18 +0530

    fetched data added new container

--- src/components/FoodContainer.jsx
+++ src/components/FoodContainer.jsx
@@ -1,36 +1,39 @@
 import { useEffect, useState } from "react"
 import styled from "styled-components"
+import ItemContainer from "./ItemContainer"
 
 export default function FoodContainer({api}) {
     
-    const [foodData, setFoodData] = useState("")
+    const [foodData, setFoodData] = useState()
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState()
     
-    useEffect(()=>{
-        setIsLoading(true)
+    async function fetchData() {
+        try {
+          const response = await fetch(api);
+          const data = await response.json();
+          setFoodData(data);
+        } catch (err) {
+          setError("Unable to Load Data");
+          console.error(`Error While Loading: ${err}`);
+        }
+        setIsLoading(false);
+    }
 
-        fetch(api).then(
-            serverPromise => {
-                serverPromise.json()
-                .then(data=> setFoodData(data))
-                setIsLoading(false)
-            }
-        ).catch(err=>{
-            setError("Unable to Load Data")
-            console.error(`Error While Loading: ${err}`)
-        })
-    })
+    useEffect(() => {
+        setIsLoading(true);
+        fetchData();
+    }, []);
+
+    
+
+    if (error) return <div>Something Went Wrong...</div>
+    if (isLoading) return <div style={{color: "white"}}>Loading...</div>
 
     return(
         <Main>
             <MainContainer>
-                <ItemContainer>
-                    <img src="" alt="" />
-                    <p className="title"></p>
-                    <p className="description"></p>
-                    <p className="price"></p>
-                </ItemContainer>
+                <ItemContainer foodData={foodData} api={api}/>
             </MainContainer>
         </Main>
     )
@@ -48,29 +51,4 @@ const MainContainer = styled.div`
     grid-template-columns: repeat(3, 1fr);
     margin-top: 5rem;
     gap: 6rem;
-`
-
-const ItemContainer = styled.div`
-    width: 12.5rem;
-    height: 13rem;
-    text-align: center;
-    background-color: var(--dark2);
-    border-radius: 1rem;
-    color: var(--white);
-
-    img {
-        width: 8rem;
-        height: 8rem;
-        border-radius: 50%;
-        background-color: var(--primary);
-        margin-top: -3rem;
-    }
-
-    .title {
-        font-weight: 600;
-    }
-
-    .description {
-        color: var(--light)
-    }
 `
---
+++ src/components/ItemContainer.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components"
+
+export default function ItemContainer({foodData, api}) {
+        return ( 
+            <>
+            {foodData.map((data, index) => (
+                <Item key={index}>
+                    <img src={`http://localhost:8000${data.image}`} alt="" />
+                    <p className="title">{data.name}</p>
+                    <p className="description">{data.text}</p>
+                    <p className="price"><span class="material-symbols-outlined">currency_rupee</span>
+                    {data.price}</p>
+                </Item>
+            ))
+            }  
+            </>
+             
+        )
+}
+
+const Item = styled.div`
+    width: 15rem;
+    height: 14rem;
+    text-align: center;
+    background-color: var(--dark2);
+    border-radius: 1rem;
+    color: var(--white);
+    cursor: pointer;
+
+    img {
+        width: 8rem;
+        height: 8rem;
+        border-radius: 50%;
+        background-color: var(--primary);
+        margin-top: -3rem;
+        object-fit: cover;
+    }
+
+    .title {
+        font-weight: 600;
+    }
+
+    .description {
+        color: var(--light);
+        font-size: 0.85rem;
+    }
+
+    .price span {
+        font-size: .9rem;
+        color: var(--light);
+    }
+`
